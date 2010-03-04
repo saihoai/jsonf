5b4adc594201f2fe7d519602df50b82b9d394f2f
jeoffwilks <jeoffwilks@466424ac-1cc8-11df-b00d-0d7d7fff8fea>
2010-03-04 03:36:58 +0000

    jsonf now supports checkboxes and radio buttons (fixes #1, refs #3)

--- jquery.jsonf.js
+++ jquery.jsonf.js
@@ -14,6 +14,14 @@
 			sel_object_or_array = sel_object + ',' + sel_array,
 			formroot = this;
 		
+		function fieldval($field) {
+			var type = $field.attr('type'); 
+			if(type=='checkbox') return $field.is(':checked');
+			if(type=='radio' && !$field.is(':checked')) return undefined;
+			//if(type=='radio') return $field.attr('checked'); 
+			return $field.val();
+		}
+		
 		function serialize() {
 			var formobjects = $(formroot).find(sel_object).each(function(){ $(this).data(opts.marker,{}) });
 			var formarrays = $(formroot).find(sel_array).each(function(){ $(this).data(opts.marker,[]) });
@@ -21,16 +29,17 @@
 			var rootobj = {};
 			var hits = $(formroot).find(opts.fields).add(formobjects).add(formarrays);
 			hits.each(function(){
-				var name, val;
-				if( $(this).is(sel_object_or_array) ) {
+				var $hit = $(this), name, val;
+				if( $hit.is(sel_object_or_array) ) {
 					name = opts.getname.apply(this);
-					val = $(this).data(opts.marker);
+					val = $hit.data(opts.marker);
 				} else {
-					name = $(this).attr('name');
-					val = $(this).val();
+					name = $hit.attr('name');
+					val = fieldval($hit);
+					if(val==undefined) return; //abstain (e.g. unselected radio button)
 				}
 		
-				var parent = $(this).parentsUntil(formroot).filter(sel_object_or_array).eq(0);
+				var parent = $hit.parentsUntil(formroot).filter(sel_object_or_array).eq(0);
 				var parentobj = parent.length ? parent.data(opts.marker) : rootobj;
 				if($.isArray(parentobj)) {
 					parentobj.push(val);
