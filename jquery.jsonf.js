51945e06078b5183838268834bd889a9c87e5ea1
jeoffwilks <jeoffwilks@466424ac-1cc8-11df-b00d-0d7d7fff8fea>
2010-03-10 17:12:23 +0000

    jsonf() now works properly on object subtrees; also added a <select> element to demo page

--- jquery.jsonf.js
+++ jquery.jsonf.js
@@ -9,12 +9,11 @@
 		};
 		opts = $.extend(defaults, opts);
 		
-		var sel_object = '.' + opts.marker,
-			sel_array = '.' + opts.marker + '-array',
-			sel_object_or_array = sel_object + ',' + sel_array,
-			formroot = this;
+		if(!opts.sel_object) opts.sel_object = '.' + opts.marker;
+		if(!opts.sel_array) opts.sel_array = '.' + opts.marker + '-array';
+		opts.sel_object_or_array = opts.sel_object + ',' + opts.sel_array;
 		
-		function fieldval($field) {
+		var fieldval = function($field) {
 			var type = $field.attr('type'); 
 			if(type=='checkbox') return $field.is(':checked');
 			if(type=='radio' && !$field.is(':checked')) return undefined;
@@ -22,35 +21,37 @@
 			return $field.val();
 		}
 		
-		function serialize() {
-			var formobjects = $(formroot).find(sel_object).each(function(){ $(this).data(opts.marker,{}) });
-			var formarrays = $(formroot).find(sel_array).each(function(){ $(this).data(opts.marker,[]) });
-		
-			var rootobj = {};
-			var hits = $(formroot).find(opts.fields).add(formobjects).add(formarrays);
-			hits.each(function(){
+		var serialize = function() {
+			var $formroot = $(this);
+			$formroot.data(opts.marker, $formroot.is(opts.sel_array) ? [] : {});
+			var formobjects = $formroot.find(opts.sel_object).each(function(){ $(this).data(opts.marker,{}) });
+			var formarrays = $formroot.find(opts.sel_array).each(function(){ $(this).data(opts.marker,[]) });
+			
+			var $hits = $formroot.find(opts.fields).add(formobjects).add(formarrays);
+			$hits.each(function(){
 				var $hit = $(this), name, val;
-				if( $hit.is(sel_object_or_array) ) {
+				if( $hit.is(opts.sel_object_or_array) ) {
 					name = opts.getname.apply(this);
 					val = $hit.data(opts.marker);
 				} else {
-					name = $hit.attr('name');
+					name = $hit.is('[name]') && $hit.attr('name') || undefined;
 					val = fieldval($hit);
-					if(val==undefined) return; //abstain (e.g. unselected radio button)
+					if(val===undefined) return; //abstain (e.g. unselected radio button)
 				}
-		
-				var parent = $hit.parentsUntil(formroot).filter(sel_object_or_array).eq(0);
-				var parentobj = parent.length ? parent.data(opts.marker) : rootobj;
+				
+				var $parent = $hit.parentsUntil($formroot).filter(opts.sel_object_or_array).eq(0);
+				if(!$parent.length) $parent = $formroot;
+				var parentobj = $parent.data(opts.marker);
 				if($.isArray(parentobj)) {
 					parentobj.push(val);
-				} else {
+				} else if(name) {
 					parentobj[name] = val;
 				}
 			});
-			return rootobj;
+			return $formroot.data(opts.marker);
 		};
 		
-		function load(formroot) {
+		var load = function(formroot) {
 			//TODO implement loading from JSON into form elements
 			return this;
 		};
